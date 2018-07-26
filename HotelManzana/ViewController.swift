9f9b663d435b70006fd481325f23988a5d8798c6
Artem Sviridov <svyrydovartem@gmail.com>
2018-07-26 18:52:08 +0300

    Initial Commit

---
+++ HotelManzana/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotelManzana
+//
+//  Created by Artem Sviridov on 26.07.2018.
+//  Copyright © 2018 Artem Sviridov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
